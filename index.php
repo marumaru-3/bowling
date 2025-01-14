7d685bfde7e2362a8d056959b98fd5da16bf14a0
marumaru <yukinaka3009@gmail.com>
2025-01-14 16:02:55 +0900

    見た目の実装完了

--- index.php
+++ index.php
@@ -59,7 +59,7 @@
       class="bowling-visual">
       <div class="bowling-ball"></div>
       <div class="bowling-result">
-        <p>Strike!!!</p>
+        <p></p>
       </div>
     </div>
     <div class="btn-group">
@@ -78,6 +78,7 @@
       const throwScore = document.querySelector('#score-board .throw-score');
       const totalScore = document.querySelector('#score-board .total-score');
       const bowlingVisual = document.getElementById('bowling-visual');
+      const bowlingResultText = document.querySelector('.bowling-result p');
 
       // 最初にスコアボードを表示
       fetch('getGameData.php', {
@@ -91,6 +92,7 @@
           // ゲーム終了判定
           if (data.isGameOver) {
             throwBtn.textContent = 'ゲーム終了';
+            throwBtn.classList.add('game-over');
             throwBtn.disabled = true; // ボタンを無効化
           }
         })
@@ -132,12 +134,17 @@
           })
           .then(response => response.json())
           .then(data => {
+            // ビジュアルアニメーションに投球結果を表示
+            resultScoreVisual(data.frames);
             // スコアボードを更新
-            updateScoreBoard(data.frames);
+            setTimeout(() => {
+              updateScoreBoard(data.frames);
+            }, 1300)
 
             // ゲーム終了判定
             if (data.isGameOver) {
               throwBtn.textContent = 'ゲーム終了';
+              throwBtn.classList.add('game-over');
               throwBtn.disabled = true; // ボタンを無効化
             }
           })
@@ -161,6 +168,9 @@
             updateScoreBoard(data.frames);
 
             throwBtn.textContent = 'ボールを投げる';
+            if (throwBtn.classList.contains('game-over')) {
+              throwBtn.classList.remove('game-over');
+            }
             throwBtn.disabled = false; // 投げるボタンを有効化
           })
       })
@@ -168,15 +178,54 @@
       // スコアボードをHTMLに描画
       const updateScoreBoard = (frames) => {
         frames.forEach((frame, frameIndex) => {
-          throwScore.children[(frameIndex + 1) * 2 - 2].innerHTML = frame.firstThrow ?? '-';
-          throwScore.children[(frameIndex + 1) * 2 - 1].innerHTML = frame.secondThrow ?? '-';
+          throwScore.children[(frameIndex + 1) * 2 - 2].innerHTML = frame.firstThrow
+
+          throwScore.children[(frameIndex + 1) * 2 - 1].innerHTML = frame.secondThrow;
 
           if (frameIndex === 9) {
-            throwScore.children[(frameIndex + 1) * 2].innerHTML = frame.thirdThrow ?? '-';
+            throwScore.children[(frameIndex + 1) * 2].innerHTML = frame.thirdThrow;
           };
 
           totalScore.children[frameIndex].innerHTML = frame.total;
         });
+      };
+      // ビジュアルアニメーションに何本倒したか描画
+      const resultScoreVisual = (frames) => {
+        const notNullScoreFrame = frames.filter((frame, frameIndex) => {
+          if (frame.firstThrow !== null || frame.secondThrow !== null || frame.thirdThrow !== null) {
+            return frame;
+          }
+        });
+        const latestFrameScore = Object.values(notNullScoreFrame[notNullScoreFrame.length - 1]).filter((score,
+          scoreIndex) => {
+          return scoreIndex < 3 && score !== null;
+        });
+
+        const latestScore = latestFrameScore[latestFrameScore.length - 1];
+
+        if (notNullScoreFrame.length === 10) {
+          if (latestFrameScore[0] === 10 && !latestFrameScore[1] ||
+            latestFrameScore[0] === 10 && latestFrameScore[1] === 10 ||
+            latestFrameScore[2] === 10) {
+            bowlingResultText.innerHTML = "ストライク!!";
+          } else if (latestFrameScore[0] !== 10 &&
+            latestFrameScore[0] + latestFrameScore[1] === 10 && !latestFrameScore[2] ||
+            latestFrameScore[1] + latestFrameScore[2] === 10) {
+            bowlingResultText.innerHTML = "スペア!!";
+          } else {
+            bowlingResultText.innerHTML = `<span class="pin-num">${latestScore}</span>本ヒット`;
+          }
+        } else {
+          if (latestFrameScore[0] === 10 ||
+            latestFrameScore[0] === 10) {
+            bowlingResultText.innerHTML = "ストライク!!";
+          } else if (latestFrameScore[0] + latestFrameScore[1] === 10) {
+            bowlingResultText.innerHTML = "スペア!!";
+          } else {
+            bowlingResultText.innerHTML = `<span class="pin-num">${latestScore}</span>本ヒット`;
+          }
+        }
+
       };
     });
   </script>
